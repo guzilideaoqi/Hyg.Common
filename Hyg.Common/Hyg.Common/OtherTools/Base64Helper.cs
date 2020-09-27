314bd18a848cb9b40f784bd2146b6fb0cfd87f27
yaguang hu <huyaguang92@hotmail.com>
2020-09-27 18:25:05 +0800

    1、增加base64类

---
+++ Hyg.Common/Hyg.Common/OtherTools/Base64Helper.cs
@@ -0,0 +1,52 @@
+﻿/*===================================Copyright© 2020 xxx Ltd. All rights reserved.==============================
+
+文件类名 : Base64Helper.cs
+创建人员 : Mr.Hu
+创建时间 : 2020-09-27 14:44:48 
+备注说明 : 
+
+ =====================================End=======================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hyg.Common.OtherTools
+{
+    /// <summary>
+    /// Base64Helper
+    /// </summary>
+    public class Base64Helper
+    {
+        ///编码
+        public static string EncodeBase64(string code, string code_type = "utf-8")
+        {
+            string encode = "";
+            byte[] bytes = Encoding.GetEncoding(code_type).GetBytes(code);
+            try
+            {
+                encode = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                encode = code;
+            }
+            return encode;
+        }
+        ///解码
+        public static string DecodeBase64(string code_type, string code)
+        {
+            string decode = "";
+            byte[] bytes = Convert.FromBase64String(code);
+            try
+            {
+                decode = Encoding.GetEncoding(code_type).GetString(bytes);
+            }
+            catch
+            {
+                decode = code;
+            }
+            return decode;
+        }
+    }
+}
